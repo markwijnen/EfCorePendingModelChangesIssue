dc5e24a6ca5edecd9cab018ce46ec7169dad45c1
Mark <>
2026-02-12 15:30:42 +0100

    add demo project with efcore 9.0.13

---
+++ EfCorePendingModelChangesIssue/Migrations/20260212142917_EfCore09.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCorePendingModelChangesIssue.Migrations
+{
+    /// <inheritdoc />
+    public partial class EfCore09 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Entities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value_Value = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entities", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entities");
+        }
+    }
+}
---
+++ EfCorePendingModelChangesIssue/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using var db = new DemoDbContext();
+await db.Database.MigrateAsync();
+
+class DemoDbContext : DbContext
+{
+    public DbSet<DemoEntity> Entities { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<DemoEntity>().ComplexProperty(e => e.Value);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite("Data Source=demo.sqlite");
+    }
+}
+
+class DemoEntity
+{
+    public int Id { get; set; }
+    public DemoValueObject Value { get; set; }
+}
+
+struct DemoValueObject
+{
+    public int Value { get; set; }
+}
