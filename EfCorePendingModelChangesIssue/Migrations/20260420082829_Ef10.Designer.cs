3dcbdd17a9da8e8c168e245a9ee2eeb715a230ee
Mark <>
2026-04-20 10:29:15 +0200

    add migration with dotnet-ef tool 10.0.6

--- EfCorePendingModelChangesIssue/Migrations/20260420082829_Ef10.Designer.cs
+++ EfCorePendingModelChangesIssue/Migrations/20260420082829_Ef10.Designer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EfCorePendingModelChangesIssue.Migrations
 {
     [DbContext(typeof(DemoDbContext))]
-    partial class DemoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260420082829_Ef10")]
+    partial class Ef10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7-dev");
