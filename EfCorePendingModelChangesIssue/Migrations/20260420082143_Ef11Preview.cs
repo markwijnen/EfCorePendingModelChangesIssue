e74c14c9d4da56c2056620e63ae39fb5115b4036
Mark <>
2026-04-20 10:23:48 +0200

    add migrations with ef core 11.0.0-preview.4.26215.121

---
+++ EfCorePendingModelChangesIssue/Migrations/20260420082143_Ef11Preview.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCorePendingModelChangesIssue.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20260420082143_Ef11Preview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
